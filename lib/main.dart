a0ab7b706bd03da2cd0e7642f45db15c0bb6727f
RhManambina <manambinafanomezantsoa@gmail.com>
2024-03-11 14:16:40 +0100

    form

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:mini/formulaire.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const Formulairekely(),
-//     );
-//   }
-// }
-
-// //
-
 import 'package:flutter/material.dart';
 import 'package:mini/formulaire.dart';
 
@@ -35,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const appTitle = 'Form Styling Demo';
     return MaterialApp(
+      // routes: ,
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(
---
+++ lib/tableauAffichage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TableauAffichage extends StatelessWidget {
+  const TableauAffichage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text('data')],
+    );
+  }
+}
